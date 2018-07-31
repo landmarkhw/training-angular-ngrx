d188d95bf854cb921a641dedf62ea61c94f66e51
Doug Day <dougrday@gmail.com>
2018-07-31 10:47:33 -0600

    Add settings.json for peep extension Cleanup unused imports

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { MovieActions } from "../../actions/movie.actions";
 import { SearchResult } from "../../models/themoviedb";
 import { AppState } from "../../reducers";
-import { getSearchResultList, getSelectedMovie } from "../../selectors/movie.selectors";
+import { getSearchResultList } from "../../selectors/movie.selectors";
 
 @Component({
     selector: "app-movie-list",
